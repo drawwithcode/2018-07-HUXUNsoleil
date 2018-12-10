fb1332758ac1ff104d1fbf6cfe9259a5ddc465a5
HUXUNsoleil <43408081+HUXUNsoleil@users.noreply.github.com>
2018-12-10 13:22:09 +0100

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,11 +1,31 @@
-function preload(){
-  // put preload code here
-}
+var capture;
+var mic;
 
 function setup() {
-  // put setup code here
+  createCanvas(1280, 960);
+  capture = createCapture(VIDEO);
+  capture.size(1280, 960);
+  capture.hide();
+
+  mic = new p5.AudioIn();
+
+  // start the Audio Input.
+  mic.start();
 }
 
 function draw() {
-  // put drawing code here
+
+  var myImage = capture.loadPixels();
+
+  background(0);
+  var vol = mic.getLevel();
+  
+
+  for (var x = 0; x < 1280; x += 10) {
+    for (var y = 0; y < 960; y += 10) {
+      fill(myImage.get(x, y));
+
+      ellipse(x, y, vol * 200);
+    }
+  }
 }
